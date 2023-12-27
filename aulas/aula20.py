d67ae4df129c2837f14bfd759b568cd4051b31f5
naralicecosta <naralicecosta04@gmail.com>
2023-12-26 21:36:31 -0300

    aula sobre loop for

--- aulas/aula20.py
+++ aulas/aula20.py
@@ -1,8 +1,14 @@
-texto = 'Python'
+'''texto = 'Python'
 
 i = 0
 tamanho_string = len(texto)
 
 while i < tamanho_string:
     print(texto[i], i)
-    i += 1
+    i += 1'''
+texto = 'Python'
+novo_texto = ''
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
