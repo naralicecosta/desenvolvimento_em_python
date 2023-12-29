938fd55e4158d7d6ca7b845f0bbc7bd382b7d702
naralicecosta <naralicecosta04@gmail.com>
2023-12-29 13:48:12 -0300

    exercicio validação cpf

--- exercicios/validar_cpf.py
+++ exercicios/validar_cpf.py
@@ -1,6 +1,6 @@
-cpf = '96348488004'
+cpf_enviado_usuario = '96348488004'
 
-nove_digitos = cpf[:9]
+nove_digitos = cpf_enviado_usuario[:9]
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
@@ -11,5 +11,21 @@ for digito_1  in nove_digitos:
 digito_1 = (resultado_digito_1 *10 ) %11
 digito_1 = digito_1 if digito_1 <= 9 else 0
 print(digito_1)
-    
+
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2  = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+if cpf_enviado_usuario == cpf_gerado_calculo:
+    print(f'{cpf_enviado_usuario} é valido')
+else:
+    print('CPF É VÁLIDO')
     
