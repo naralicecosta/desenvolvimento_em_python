9a0612d568c6b6ccba00669af31997a5fd2bc8e1
naralicecosta <naralicecosta04@gmail.com>
2024-03-02 20:56:59 -0300

    docs: salvando python em json

---
+++ python_intermediario/aulas/aula36.py
@@ -0,0 +1,17 @@
+import json
+#salvando dados oython em JSON com modulo json
+pessoa = {
+    'nome': 'Naralice',
+    'sobrenome': 'Cavalcante',
+    'enderecos': [
+        {'rua': 'R1', 'numero': 33},
+        {'rua': 'R2', 'numero': 32},
+    ],
+    'altura': 1.67,
+    'numeros_preferidos': (2, 4, 5,6, 7),
+    'dev': True,
+    'nada': None,
+}
+
+with open('aula36.json', 'w') as arquivo:
+    json.dump(pessoa, arquivo, ensure_ascii=False, indent=2 )
