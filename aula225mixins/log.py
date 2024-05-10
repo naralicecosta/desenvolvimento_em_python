f36c9a4763e706636909584c7c2ed1947d6a1574
naralicecosta <naralicecosta04@gmail.com>
2024-05-10 19:35:31 -0300

    part1 mixins

---
+++ aula225mixins/log.py
@@ -0,0 +1,13 @@
+class Log:
+    def log(self, msg):
+        raise NotImplementedError('Implemente o método log')
+
+
+class LogFileMixin(Log):
+    def log(self, msg):
+        print(msg)
+
+
+if __name__ == '__main__':
+    l = LogFileMixin()
+    l.log('qualquer coisa')
