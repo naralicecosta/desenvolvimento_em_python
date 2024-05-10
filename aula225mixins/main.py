aea11325a543bd7c13d81ce274a2bca60f8bbea9
naralicecosta <naralicecosta04@gmail.com>
2024-05-10 20:48:15 -0300

    outros exemplos com herança multipla

--- aula225mixins/main.py
+++ aula225mixins/main.py
@@ -1,8 +1,16 @@
-from log import LogFileMixin, LogPrintMixin
+# from log import LogFileMixin, LogPrintMixin
 
-lp = LogPrintMixin()
-lp.log_error('qualquer coisa')
-lp.log_success('que legal')
-lf = LogFileMixin()
-lf.log_error('qualquer coisa')
-lf.log_success('que legal')
+# lp = LogPrintMixin()
+# lp.log_error('qualquer coisa')
+# lp.log_success('que legal')
+# lf = LogFileMixin()
+# lf.log_error('qualquer coisa')
+# lf.log_success('que legal')
+
+from eletronico import Smartphone
+
+galaxy_s = Smartphone('Galaxy S')
+iphone = Smartphone('Iphone')
+
+galaxy_s.ligar()
+iphone.desligar()
