6b5d7c6d31526be07cc378da92a6f3d7bb6b03b7
naralicecosta <naralicecosta04@gmail.com>
2024-05-25 13:51:23 -0300

    special methods

--- prog_orientada_objetos/exercicios/special_methods.py
+++ prog_orientada_objetos/exercicios/special_methods.py
@@ -14,4 +14,22 @@
 # __truediv__(self,other) - self / other
 # __neg__(self) - -self
 # __str__(self) - str
-# __repr__(self) - str
+# __repr__(self) - str //como o objeto seria remontado no python
+
+class Ponto:
+    def __init__(self, x, t):
+        self.x = x
+        self.y = y
+        
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+        
+    def __repr__(self):
+        # class_name = self.__class__.__name__
+        class_name = type(self).__name__
+        return f'{class_name}(x={self.x}, y={self.y})'
+
+p1 = Ponto(1, 2)
+p1 = Ponto(978, 876)
+print(p1)
+print(p2)
